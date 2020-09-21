86841d9fb086f9a46435516dbdba785eedb5c6e5
Brendhon <brendhon.e.c.m@gmail.com>
2020-09-21 08:42:55 -0300

    atualizando: Mudando local do arquivo data.json

--- modules/data/fileRead.py
+++ modules/data/fileRead.py
@@ -3,7 +3,7 @@ import json
 def readJson():
     
     # Lendo o arquivo JSON
-    with open('modules/data/data.json') as json_data:
+    with open('data.json') as json_data:
         constants = json.load(json_data)
         json_data.close()
     
